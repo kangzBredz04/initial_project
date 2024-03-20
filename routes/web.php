33bce330e13d2038a45cc0301f66ea83c07d97d9
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-20 14:16:28 +0700

    Membuat beberapa route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return "Respon ini diterima dari path / dengan metode GET";
+});
+
+Route::get('/books', function () {
+    return "Router ini nantinya akan digunakan untuk mengambil semua data buku";
+});
+
+Route::post('/books', function () {
+    return "Router ini nantinya akan digunakan untuk mencipatakan data buku yang baru";
+});
+
+Route::put('/books', function () {
+    return "Router ini nantinya akan digunakan untuk mengubah data buku";
+});
+
+Route::delete('/books', function () {
+    return "Router ini nnantinya digunakan untuk menghapus data buku";
 });
